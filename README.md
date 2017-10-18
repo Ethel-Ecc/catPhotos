4d67a11042047e0d7df0b881a87f499ee556b71e
Ethel <ethelbert@ethel.consulting.com>
2017-10-18 16:47:45 +0200

    updates on main.js and index files

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is an app for all cat lovers. It utilizes the the pixabay API functionality to retrieve high quality cat photos.
 Frameworks such as bootstrap v4.0.0 and jquery adds an even friendly UX to the design.
 Each displayed image comes with functionalities for likes, favourites and views.
-Users are able to make actions based on this functionalities add comments, view enlarged fullHD images based on 
+Users are able to make actions based on this functionalities, add comments, and view enlarged fullHD images based on 
 their choice.
 
 To all the cat lovers and cat sympathizers: "Viel Spaß!"
--- main.js
+++ main.js
@@ -2,32 +2,59 @@ $(document).ready(function (){
     "use strict";
     let ajaxResult = "";
     let counter = 0;
+
+
     //API call
-    let URL = "https://pixabay.com/api/?key=6746448-134d21a799f0124eb887bf023&q=cats&per_page=5&image_type=all";
+    let URL = "https://pixabay.com/api/?key=6746448-134d21a799f0124eb887bf023&q=cats&per_page=200&image_type=photos";
     $.getJSON(URL, function(xyz){
         if(xyz.hasOwnProperty("hits")){
             ajaxResult = xyz["hits"];
         }
-        // Displays a default image once page finish loading
-        $(".message").html(`<img class="thumbnail" src="${ajaxResult[counter].previewURL}">`);
+        // Displays a default image once on startup page load
+        $(".message").html(`
+                <div class="col-sm-3">
+                    <img class="card-img-top imgStyle" data-toggle="tooltip" data-placement="top" title="${ajaxResult[counter].tags}" 
+                    src="${ajaxResult[counter].webformatURL}">
+                </div>
+                <div class="col-sm-3">
+                    <p><strong>Image Views:  </strong>   ${ajaxResult[counter].views}</p>
+                    <p><strong> Favourites:  </strong> ${ajaxResult[counter].favorites}</p>
+                    <p><strong>Likes:  </strong> ${ajaxResult[counter].likes}</p>
+                    <p>See full description <a href="${ajaxResult[counter].pageURL}" target="_blank">here</a></p>
+                </div>
+        `);
     });
-
     //allows the user to click to see more cat images one at a time.
     $("#nextImage").on("click", function () {
         counter++;
-        if(counter <= 4){
-            $(".message").html(`<img class="thumbnail" src="${ajaxResult[counter].previewURL}">`);
+        if(counter <= 199){
+            $(".message").html(`
+                <div class="col-sm-3">
+                    <img class="card-img-top" data-toggle="tooltip" data-placement="top" title="${ajaxResult[counter].tags}"
+                     src="${ajaxResult[counter].webformatURL}">
+                </div>
+                <div class="col-sm-3">
+                    <p><strong>Image Views:  </strong>  ${ajaxResult[counter].views}</p>
+                    <p><strong> Favourites:  </strong> ${ajaxResult[counter].favorites}</p>
+                    <p><strong>Likes:  </strong>  ${ajaxResult[counter].likes}</p>
+                    <p>See full description <a href="${ajaxResult[counter].pageURL}" target="_blank">here</a></p>
+                </div>
+            `);
         } else{
             // reset counter once the default per_page count is exhausted.
             counter = 0;
-            $(".message").html(`<img class="thumbnail" src="${ajaxResult[counter].previewURL}">`);
+            $(".message").html(`
+                <div class="col-sm-3">
+                    <img class=" card-img-top" data-toggle="tooltip" data-placement="top" title="${ajaxResult[counter].tags}"
+                     src="${ajaxResult[counter].webformatURL}">
+                </div>
+                <div class="col-sm-3">
+                    <p><strong>Image Views:  </strong> ${ajaxResult[counter].views}</p>
+                    <p><strong> Favourites:  </strong> ${ajaxResult[counter].favorites}</p>
+                    <p><strong>Likes:  </strong> ${ajaxResult[counter].views}</p>
+                    <p>See full description <a href="${ajaxResult[counter].pageURL}" target="_blank">here</a></p>
+                </div>
+            `);
         }
     });
-
-    //Get the user Location via the Geolocation API calls
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function (position) {
-            $("#getMessage").html("Your Latitude: "+position.coords.latitude+"<br>Your Longitude: "+position.coords.longitude);
-        });
-    }
 });
